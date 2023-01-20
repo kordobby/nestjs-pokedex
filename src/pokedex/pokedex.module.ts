3618111d511db18e6f21b7dde0cc43ce4f2872e5
Yoon Lee <poordobby@naver.com>
2023-01-20 23:59:00 +0900

    [FEAT] add module, controller, service

---
+++ src/pokedex/pokedex.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PokedexController } from './pokedex.controller';
+import { PokedexService } from './pokedex.service';
+
+@Module({
+  imports: [],
+  controllers: [PokedexController],
+  providers: [PokedexService],
+})
+export class PokedexModule {}
